f1b7b3fa3fe41367bc386c46f5d55141ddf595ea
Donglecturer <Dongbh@fpt.edu.vn>
2022-08-16 08:01:00 +0700

    commit by dongbh

---
+++ PRN_3W_OOP_GENERIC_COLLECTION/Repositories/IGenericRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN_3W_OOP_GENERIC_COLLECTION.Repositories;
+
+public interface IGenericRepo<E>
+{
+    void Add(E e);
+    void Update(E e);
+    void Delete(E e);
+    E GetById(int id);
+    IEnumerable<E> GetAll();
+}
---
+++ PRN_3W_OOP_GENERIC_COLLECTION/Repositories/PersonRepo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PRN_3W_OOP_PRACTICE.OOP.GenericCollection;
+
+namespace PRN_3W_OOP_GENERIC_COLLECTION.Repositories;
+
+public class PersonRepo : IGenericRepo<Person>
+{
+    private IList<Person> _persons;
+    public PersonRepo()
+    {
+        _persons = new List<Person>();
+    }
+    
+    public void Add(Person e)
+    {
+       if(e != null) this._persons.Add(e);
+    }
+
+    public void Delete(Person e)
+    {
+       this._persons.Remove(e);
+    }
+
+    public IEnumerable<Person> GetAll()
+    {
+        return _persons;
+    }
+
+    public Person GetById(int id)
+    {
+        foreach (Person p in this._persons)
+        {
+            if (p.Id == id) { return p; }
+
+        }
+        return null;
+    }
+
+    public void Update(Person e)
+    {
+        //
+    }
+}
