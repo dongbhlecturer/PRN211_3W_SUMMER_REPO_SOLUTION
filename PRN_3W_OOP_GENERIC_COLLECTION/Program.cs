64518e0cded099efc3083f90d943c7c7c160ac95
Donglecturer <Dongbh@fpt.edu.vn>
2022-08-16 10:08:33 +0700

    Commit

--- PRN_3W_OOP_GENERIC_COLLECTION/Program.cs
+++ PRN_3W_OOP_GENERIC_COLLECTION/Program.cs
@@ -14,14 +14,14 @@ e1.Id = 1;
 e1.Name = "Bui Huu Dong";
 e1.Address = new Address(10, "CMT8", "P.13", "Quan 10", "HCMC");
 e1.Manday = 25;
-e1.BaseSalary = 250000;
+e1.BaseSalary = 250_000;
 
 Renter e2 = new Renter();
 e2.Id = 2;
 e2.Name = "Nguyen Cong Phuong";
 e2.Address = new Address(123, "Huynh van Banh", "P.Cong Vi", "Quan 3", "HCMC");
-e2.NetSalary = 6000000;
-e2.AddSalary = 150000;
+e2.NetSalary = 6_000_000;
+e2.AddSalary = 150_000;
 
 Employee e3 = new Employee();
 e3.Id = 1;
@@ -29,7 +29,7 @@ e3.Name = "Pack Hang Seo";
 e3.Address = new Address(11, "Phan Dinh Phung", "P.Quan Hoa", "Quan Cau Giay",
     "Ha Noi");
 e3.Manday = 30;
-e3.BaseSalary = 450000;
+e3.BaseSalary = 450_000;
 
 /*2. Dua 3 object tren vao mot list*/
 
@@ -37,10 +37,44 @@ var lstPerson = new List<Person> { e1, e2, e3 };//vua khai bao lai vua khoi tao
 /*3. Sap xep tang dan theo luong*/
 //lstPerson.Sort(new PersonComparer());
 /*4. Demo lambda: khong can dinh nghia them Comparer nhu ở mục 3 nữa mà truyền động*/
-lstPerson.Sort((a, b) => (-a.Name.Length + b.Name.Length));
+
+static int ComparePerson(Person x, Person y)
+{
+    return -x.Name.Length + y.Name.Length;
+}
+
+lstPerson.Sort(ComparePerson);
+
+
+
 
 
 foreach (Person p in lstPerson)
 {
     Console.WriteLine($"[Name: {p.Name} - Salary:{p.CalculatorSalary()}]");
-}   
+}
+
+Console.WriteLine("***********************************");
+/*Demo ve Lambda Expresion*/
+
+/*Dung Lambda standard de sap xep tang dan theo luong*/
+
+var data = lstPerson.OrderBy(p => p.CalculatorSalary());
+foreach (Person p in data)
+{
+    Console.WriteLine($"[Name: {p.Name} - Salary:{p.CalculatorSalary()}]");
+}
+/*Tinh tong luong cua n Person va in ra man hinh*/
+
+double sumSalary = lstPerson.Sum(p=>p.CalculatorSalary());
+Console.WriteLine($"Tong luong cua {lstPerson.Count} emps are: {sumSalary}");
+
+Console.WriteLine("***********************************");
+/*Dung LINQ to Object (cu phap moi) de truy van vao mot tap hop*/
+var dataPerson = from p in lstPerson 
+                 where p.Name.ToUpper().Contains("SEO")
+                 select p;
+foreach (Person p in dataPerson)
+{
+    Console.WriteLine($"[Name: {p.Name} - Salary:{p.CalculatorSalary()}]");
+}
